80eba0ec58ded10a83b72bc1b65ce7aee5305683
tBrause <brause.torsten@gmail.com>
2025-06-12 22:43:19 +0200

    Füge vollständige Implementierung des SDO-Lesevorgangs hinzu

--- tools/eta_sdo_read_state/eta_sdo_read_state.cpp
+++ tools/eta_sdo_read_state/eta_sdo_read_state.cpp
@@ -12,22 +12,24 @@
 #include <ctime>
 
 // State-Interpretation
-std::string stateToText(uint32_t state) {
+std::string stateToText(uint32_t state)
+{
     if (state == 0x8080)
-    return "Ready-to-Attach";
+        return "Ready-to-Attach";
     if (state == 0x80C0)
-    return "Normal Operation";
+        return "Normal Operation";
     if (state == 0x8040)
-    return "Do-Not-Attach";
+        return "Do-Not-Attach";
     if (state == 0x4040)
-    return "Comp. Check / Do-Not-Attach";
+        return "Comp. Check / Do-Not-Attach";
     std::ostringstream oss;
     oss << "Unbekannter State (0x" << std::hex << std::uppercase << state << ")";
     return oss.str();
 }
 
-bool readSDO(int s, int node_id, uint16_t index, uint8_t subindex, uint32_t &value, uint8_t &resp_code) {
-    struct can_frame frame {};
+bool readSDO(int s, int node_id, uint16_t index, uint8_t subindex, uint32_t &value, uint8_t &resp_code)
+{
+    struct can_frame frame{};
     frame.can_id = 0x600 + node_id;
     frame.can_dlc = 8;
     frame.data[0] = 0x40;
@@ -39,26 +41,32 @@ bool readSDO(int s, int node_id, uint16_t index, uint8_t subindex, uint32_t &val
     frame.data[6] = 0x00;
     frame.data[7] = 0x00;
 
-    if (write(s, &frame, sizeof(frame)) != sizeof(frame)) {
+    if (write(s, &frame, sizeof(frame)) != sizeof(frame))
+    {
         perror("write");
         return false;
     }
 
-    for (int i = 0; i < 80; ++i) {
-        struct can_frame resp {};
+    for (int i = 0; i < 80; ++i)
+    {
+        struct can_frame resp{};
         int nbytes = read(s, &resp, sizeof(resp));
-        if (nbytes < 0) {
+        if (nbytes < 0)
+        {
             perror("read");
             break;
         }
-        if (resp.can_id == 0x580 + node_id) {
+        if (resp.can_id == 0x580 + node_id)
+        {
             resp_code = resp.data[0];
-            if (resp_code == 0x43 || resp_code == 0x4B || resp_code == 0x47) {
+            if (resp_code == 0x43 || resp_code == 0x4B || resp_code == 0x47)
+            {
                 value = resp.data[4] | (resp.data[5] << 8) | (resp.data[6] << 16) | (resp.data[7] << 24);
                 usleep(150000);
                 return true;
             }
-            else if (resp_code == 0x80) {
+            else if (resp_code == 0x80)
+            {
                 usleep(150000);
                 return false;
             }
@@ -69,50 +77,58 @@ bool readSDO(int s, int node_id, uint16_t index, uint8_t subindex, uint32_t &val
     return false;
 }
 
-std::string nowTimeString() {
+std::string nowTimeString()
+{
     std::time_t t = std::time(nullptr);
     char buf[32];
     std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&t));
     return buf;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         std::cerr << "Nutzung: " << argv[0] << " NODE_ID" << std::endl;
         return 1;
     }
     int node_id = std::atoi(argv[1]);
-    if (node_id <= 0 || node_id > 127) {
+    if (node_id <= 0 || node_id > 127)
+    {
         std::cerr << "Ungueltige NODE_ID." << std::endl;
         return 1;
     }
 
-    const char* ifname = "can0";
+    const char *ifname = "can0";
     std::string logfile = "/home/pi/__ctrl_minimal/data/eta" + std::to_string(node_id) + "_sdo_state_log.txt";
 
     std::ofstream log(logfile, std::ios::app);
-    if (!log) {
+    if (!log)
+    {
         std::cerr << "Fehler beim Öffnen der Logdatei!" << std::endl;
         return 1;
     }
 
     int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    if (s < 0) {
+    if (s < 0)
+    {
         perror("Socket");
         return 1;
     }
 
-    struct ifreq ifr {};
+    struct ifreq ifr{};
     std::strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
-    if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
+    if (ioctl(s, SIOCGIFINDEX, &ifr) < 0)
+    {
         perror("ioctl");
         return 1;
     }
 
-    struct sockaddr_can addr {};
+    struct sockaddr_can addr{};
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
         perror("bind");
         return 1;
     }
@@ -129,18 +145,22 @@ int main(int argc, char* argv[]) {
     log << name << " (0x" << std::hex << index << ":" << std::dec << (int)subindex << "): ";
     std::cout << "Lese " << name << " (0x" << std::hex << index << ":" << std::dec << (int)subindex << ") ... ";
 
-    if (readSDO(s, node_id, index, subindex, value, resp_code)) {
+    if (readSDO(s, node_id, index, subindex, value, resp_code))
+    {
         log << "0x" << std::hex << std::setw(4) << std::setfill('0') << value
-        << " = " << stateToText(value) << std::endl;
+            << " = " << stateToText(value) << std::endl;
         std::cout << "0x" << std::hex << std::setw(4) << std::setfill('0') << value
-        << " = " << stateToText(value) << std::endl;
+                  << " = " << stateToText(value) << std::endl;
     }
-    else {
-        if (resp_code == 0x80) {
+    else
+    {
+        if (resp_code == 0x80)
+        {
             log << "Fehler: Kein Zugriff oder Objekt nicht vorhanden.\n";
             std::cout << "Fehler: Kein Zugriff oder Objekt nicht vorhanden." << std::endl;
         }
-        else {
+        else
+        {
             log << "Keine Antwort oder Timeout!\n";
             std::cout << "Keine Antwort oder Timeout!" << std::endl;
         }
